8408be6b4293c46963161290ea75cc2c1b1b6509
Muhammad Azrial <azrialmons@gmail.com>
2020-05-31 03:14:16 +0700

    tambah momentjs dan buat statusbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="flex justify-center w-screen h-screen bg-gray-100">
-    <div class="h-screen max-w-sm bg-white shadow-lg"></div>
+    <div class="relative w-full h-screen max-w-sm bg-white shadow-lg">
+      <statusbar />
+    </div>
   </div>
 </template>
 
 <script>
+import statusbar from "@/components/_statusbar.vue";
+
 export default {
-  name: "app"
+  name: "app",
+  components: {
+    statusbar
+  }
 };
 </script>
 

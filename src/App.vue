026c598249a02c8ba51d3f3b570da59f8c480d32
Muhammad Azrial <azrialmons@gmail.com>
2020-05-31 04:17:10 +0700

    notifikasi

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,46 @@
 <template>
-  <div class="flex justify-center w-screen h-screen bg-gray-100">
+  <div class="flex justify-center w-screen h-screen bg-gray-100 select-none">
     <div class="relative w-full h-screen max-w-sm bg-white shadow-lg">
       <statusbar class="z-50" />
       <topbar />
       <div id="__bottom" class="absolute bottom-0 w-full">
         <img src="@/assets/img/bottom.png" class="bg-center" />
       </div>
+
+      <!-- notifikasi -->
+      <div
+        v-if="notifikasi"
+        id="__notifikasi"
+        class="absolute w-full h-full bg-black bg-opacity-75"
+      >
+        <div class="container flex items-center justify-center h-full">
+          <div
+            id="__box"
+            class="flex flex-col px-5 pt-6 space-y-3 leading-none text-center"
+          >
+            <h2 class="text-2xl">Pemberitahuan</h2>
+            <span class="text-base">
+              Masukkin nama kamu – saya boleh manggil kamu apa ?
+            </span>
+            <input
+              type="text"
+              class="focus:outline-none w-full p-2 placeholder-gray-400 rounded-sm"
+              placeholder="Nama panggilan"
+              autofocus
+              v-model="nama"
+            />
+            <br />
+            <button
+              id="__btnCTA"
+              class="focus:outline-none focus:text-blue-800 py-4 -mx-5 text-lg font-medium text-blue-700 border-t border-gray-700 border-opacity-25 select-none"
+              @click="handleNama"
+            >
+              OK
+            </button>
+          </div>
+        </div>
+      </div>
+      <!-- notifikasi end -->
     </div>
   </div>
 </template>
@@ -19,8 +54,48 @@ export default {
   components: {
     statusbar,
     topbar
+  },
+
+  data() {
+    return {
+      notifikasi: true,
+      nama: ""
+    };
+  },
+
+  methods: {
+    handleNama() {
+      this.notifikasi = false;
+      console.log(this.nama);
+    }
   }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#__notifikasi {
+  backdrop-filter: blur(2.5px);
+  z-index: 999;
+
+  #__box {
+    backdrop-filter: blur(54.37px);
+    background-color: rgba($color: #f8f8f8, $alpha: 0.82);
+    border-radius: 14px;
+    font-family: "SF Text";
+
+    h2 {
+      letter-spacing: -0.0241176em;
+      font-weight: 600;
+    }
+
+    span {
+      letter-spacing: -0.00615385em;
+      font-weight: 400;
+    }
+
+    input {
+      border: 0.5px solid #8e8e93;
+    }
+  }
+}
+</style>
